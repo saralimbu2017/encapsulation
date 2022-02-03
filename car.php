17b962298340b8ff7ae2c4a6fdf70e4258aab16b
saralimbu2017 <iamsebi7@gmail.com>
2022-02-03 23:21:00 +1100

    Defining methods

--- car.php
+++ car.php
@@ -1,8 +1,16 @@
 class Car
-{
+{ 
+  //car attributes
   private $speed;
   public $color = "White";
 
-  
+  //car methods to demostrate the behaviours
+  public function accelerate($value) {
+    $this->speed += $value;
+  }
+
+  public function brake() {
+    $speed = 0;
+  }
 
 }
